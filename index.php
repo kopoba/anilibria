1d91450e9ad99998a8cf943c3514d975df82ec3a
poiuty <poiuty@lepus.su>
2018-09-23 18:05:35 +0300

    init

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+	<head>
+		<title>AniLibria.TV - Так звучит аниме! Озвучка аниме для домашнего просмотра.</title>
+		<meta name="keywords" content="Анилибрия" />
+		<meta name="description" content="Анилибрия - смотреть аниме онлайн, торрент-трекер" />
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+		<link rel="stylesheet" type="text/css" href="/css/bootstrap.min.css">
+		<link rel="stylesheet" type="text/css" href="/css/main.css">
+		<link rel="stylesheet" type="text/css" href="/css/jquery.bxslider.css">
+		<script src="/js/jquery.min.js"></script>
+		<script src="/js/popper.min.js"></script>
+		<script src="/js/bootstrap.min.js"></script>
+		<script src="/js/jquery.bxslider.js"></script>
+		<script src="/js/main.js"></script>
+	</head>
+	<body>
+		<div class="container">
+		</div>
+	</body>
+</html>
---
+++ private/config.php
@@ -0,0 +1,5 @@
+<?php
+$config['mysql_host'] = 'localhost';
+$config['mysql_user'] = 'anilibria';
+$config['mysql_passwd'] = 'anilibria';
+$config['mysql_base'] = 'anilibria';
