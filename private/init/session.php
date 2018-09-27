0929d2a4c45009a82b9582b148ec1b709f6626ea
poiuty <poiuty@lepus.su>
2018-09-27 18:43:49 +0300

    check session start

--- private/init/session.php
+++ private/init/session.php
@@ -1,5 +1,5 @@
 <?php
-//if(!isset($_SESSION)) {
-//	session_set_cookie_params(0, '/', $_SERVER['SERVER_NAME'], true, true);
-//	session_start();
-//}
+if(session_status() == PHP_SESSION_NONE){
+	session_set_cookie_params(0, '/', $_SERVER['SERVER_NAME'], true, true);
+	session_start();
+}
