da701ec34343a43d1956a08e2c94e8b04e07242f
Kirja <kirillik1993@gmail.com>
2018-10-03 17:53:22 +0300

    Header fix

--- private/header.php
+++ private/header.php
@@ -4,22 +4,22 @@
     <title>AniLibria.TV</title>
     <meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="css/slider.css">
-    <link rel="stylesheet" href="fonts/fonts.css">
-	<link rel="stylesheet" href="css/width.css">
-    <script src="js/jquery.min.js"></script>
-    <script src="js/jquery.bxslider.js"></script>
-    <script src="js/main.js"></script>
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/slider.css">
+    <link rel="stylesheet" href="../fonts/fonts.css">
+	<link rel="stylesheet" href="../css/width.css">
+    <script src="../js/jquery.min.js"></script>
+    <script src="../js/jquery.bxslider.js"></script>
+    <script src="../js/main.js"></script>
 </head>
 <body>
     <div id="content_wrapper">
         <header>
             <div id="main_header">
-                <img src="images/logo.png" alt="logo"/>
+                <img src="../images/logo.png" alt="logo"/>
                 <nav id="main_menu">
                     <div class="subnav">
-                        <a href="index.html" class="main_nav_link active">Главная</a>
+                        <a href="#" class="main_nav_link active">Главная</a>
                         <div class="subnav-content active">
                             <a href="#" class="active">Новости</a>
                             <a href="#">Календарь осеннего сезона</a>
@@ -66,7 +66,7 @@
                     </div>
                     <a href="#" class="main_nav_link">Форум</a>
                 </nav>
-                <a href="#" id="user_avatar"><img src="images/avatar.png" alt="avatar"/></a>
+                <a href="#" id="user_avatar"><img src="../images/avatar.png" alt="avatar"/></a>
             </div>
             <div id="hslider">
                 <div id="slides_main_wrapper" class="slideshow">
@@ -84,38 +84,38 @@
         </header>
         <aside id="side_menu">
             <a href="#">
-                <img src="images/subscribe_new_series.png" class="side_img_link" alt="new series subscription"/>
+                <img src="../images/subscribe_new_series.png" class="side_img_link" alt="new series subscription"/>
             </a>
 			<div id="social_links">
 				<span>Ссылки</span>
 				<a href="#">
-					<img src="images/vk.png" alt="vkontakte" />
+					<img src="../images/vk.png" alt="vkontakte" />
 				</a>
 				<a href="#">
-					<img src="images/yt.png" alt="YouTube" />
+					<img src="../images/yt.png" alt="YouTube" />
 				</a>
 				<a href="#">
-					<img src="images/tg.png" alt="Telegram" />
+					<img src="../images/tg.png" alt="Telegram" />
 				</a>
 				<a href="#">
-					<img src="images/ds.png" alt="Discord" />
+					<img src="../images/ds.png" alt="Discord" />
 				</a>
 				<a href="#">
-					<img src="images/vedroid.png" alt="Android" />
+					<img src="../images/vedroid.png" alt="Android" />
 				</a>
 			</div>
             <!-- Скрывать для действующего спонсора patreon -->
             <a href="#">
-                <img src="images/patreon_subscribe.png" class="side_img_link" alt="patreon subscription"/>
+                <img src="../images/patreon_subscribe.png" class="side_img_link" alt="patreon subscription"/>
             </a>
             <a href="#">
-                <img src="images/support_us.png" class="side_img_link" alt="support us"/>
+                <img src="../images/support_us.png" class="side_img_link" alt="support us"/>
             </a>
             <!-- Скрывать для действующего спонсора patreon -->
             <div id="notice">
                 <p>Рекламное место<br/>365 x 500 px</p>
                 <a href="#">
-                    <!--<img src="images/support_us.png" class="side_img_link" alt="ad name"/>-->
+                    <!--<img src="../images/support_us.png" class="side_img_link" alt="ad name"/>-->
                 </a>
             </div>
             <a class="twitter-timeline" data-lang="ru" data-width="365" data-height="500" data-theme="dark" data-link-color="#eb5252" href="https://twitter.com/AniLibria_Tv?ref_src=twsrc%5Etfw">Tweets by AniLibria_Tv</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
@@ -125,7 +125,7 @@
                 <p>Рекламное место<br/>639 x 150 px</p>
                 <!-- Заменять картинку на благодарность для действующего спонсора patreon -->
                 <a href="#">
-                    <!--<img src="images/support_us.png" class="side_img_link" alt="ad name"/>-->
+                    <!--<img src="../images/support_us.png" class="side_img_link" alt="ad name"/>-->
                 </a>
             </div>
             <div id="search">
