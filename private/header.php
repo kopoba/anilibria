3e7b2b61be0a3defb8b8755c6d721dfaf08bbb40
Kirjaa <kirillik1993@gmail.com>
2018-10-03 23:09:04 +0300

    header fix

--- private/header.php
+++ private/header.php
@@ -4,19 +4,19 @@
     <title>AniLibria.TV</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="css/slider.css">
-    <link rel="stylesheet" href="fonts/fonts.css">
-    <link rel="stylesheet" href="css/width.css">
-    <script src="js/jquery.min.js"></script>
-    <script src="js/jquery.bxslider.js"></script>
-    <script src="js/main.js"></script>
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/slider.css">
+    <link rel="stylesheet" href="../fonts/fonts.css">
+    <link rel="stylesheet" href="../css/width.css">
+    <script src="../js/jquery.min.js"></script>
+    <script src="../js/jquery.bxslider.js"></script>
+    <script src="../js/main.js"></script>
 </head>
 <body>
 <div id="content_wrapper">
     <header>
         <div id="main_header">
-            <img src="images/logo.png" alt="logo"/>
+            <img src="../images/logo.png" alt="logo"/>
             <nav id="main_menu">
                 <div class="subnav">
                     <a href="#" class="main_nav_link active">Главная</a>
@@ -74,7 +74,7 @@
                     </div>
                 </div>
             </nav>
-            <a href="#" id="user_avatar"><img src="images/avatar.png" alt="avatar"/></a>
+            <a href="#" id="user_avatar"><img src="../images/avatar.png" alt="avatar"/></a>
         </div>
         <div id="hslider">
             <div id="slides_main_wrapper" class="slideshow">
@@ -92,32 +92,32 @@
     </header>
     <aside id="side_menu">
         <a href="#" class="side_image_links">
-            <img src="images/subscribe_new_series.png" class="side_img_link" alt="new series subscription"/>
+            <img src="../images/subscribe_new_series.png" class="side_img_link" alt="new series subscription"/>
         </a>
         <div id="social_links">
             <span>Ссылки</span>
             <a href="#">
-                <img src="images/vk.png" alt="vkontakte" />
+                <img src="../images/vk.png" alt="vkontakte" />
             </a>
             <a href="#">
-                <img src="images/yt.png" alt="YouTube" />
+                <img src="../images/yt.png" alt="YouTube" />
             </a>
             <a href="#">
-                <img src="images/tg.png" alt="Telegram" />
+                <img src="../images/tg.png" alt="Telegram" />
             </a>
             <a href="#">
-                <img src="images/ds.png" alt="Discord" />
+                <img src="../images/ds.png" alt="Discord" />
             </a>
             <a href="#">
-                <img src="images/vedroid.png" alt="Android" />
+                <img src="../images/vedroid.png" alt="Android" />
             </a>
         </div>
         <!-- Скрывать для действующего спонсора patreon -->
         <a href="#" class="side_image_links">
-            <img src="images/patreon_subscribe.png" class="side_img_link" alt="patreon subscription"/>
+            <img src="../images/patreon_subscribe.png" class="side_img_link" alt="patreon subscription"/>
         </a>
         <a href="#" class="side_image_links">
-            <img src="images/support_us.png" class="side_img_link" alt="support us"/>
+            <img src="../images/support_us.png" class="side_img_link" alt="support us"/>
         </a>
         <!-- Скрывать для действующего спонсора patreon -->
         <div id="notice">
