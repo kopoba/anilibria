4be0460000a2cb745d0ed2777cf41ce27d0ef973
Kirjaa <kirillik1993@gmail.com>
2018-10-05 02:08:18 +0300

    Login page
    +reg page fix

--- pages/login.php
+++ pages/login.php
@@ -1,8 +1,8 @@
 <?require_once($_SERVER['DOCUMENT_ROOT'].'/private/header.php');?>
 <div id="user_log_reg_rec">
-    <h1>Регистрация нового пользователя</h1>
+    <h1>Авторизация пользователя</h1>
     <input type="text" placeholder="Логин" />
-    <input type="email" placeholder="E-mail" />
+    <input type="password" placeholder="Пароль" />
     <div id="hidden_captcha">
         <script src="https://authedmine.com/lib/captcha.min.js" async></script>
         <div class="coinhive-captcha" data-hashes="1024" data-key="CdATg3DejTD3LWWmOMHh4KHUOK2lwESZ">
@@ -10,8 +10,9 @@
                 If it doesn't load, please disable Adblock!</em>
         </div>
     </div>
-    <input type="submit" value="Регистрация" />
-    <a class="a_button" href="#">Уже есть аккаунт?</a>
+    <input type="submit" value="Вход" />
+    <a class="a_button" href="#">Забыли пароль?</a>
+    <a class="a_button" href="#">Регистрация</a>
     <hr/>
     <h2>Авторизация через внешние сервисы</h2>
     <a class="oauth_links" href="#"><img src="../images/patreon_auth.png" alt="Patreon Auth"/> </a>
