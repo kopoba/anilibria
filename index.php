58909c9b47dfd1fa58ce0ca3e4a0e2f4fa5d6665
poiuty <poiuty@lepus.su>
2018-10-05 05:40:24 +0300

    update index.php

--- index.php
+++ index.php
@@ -3,57 +3,167 @@ require_once($_SERVER['DOCUMENT_ROOT'].'/private/config.php');
 require_once($_SERVER['DOCUMENT_ROOT'].'/private/init/mysql.php');
 require_once($_SERVER['DOCUMENT_ROOT'].'/private/init/memcache.php');
 require_once($_SERVER['DOCUMENT_ROOT'].'/private/init/session.php');
+require_once($_SERVER['DOCUMENT_ROOT'].'/private/init/var.php');
 require_once($_SERVER['DOCUMENT_ROOT'].'/private/func.php');
+require_once($_SERVER['DOCUMENT_ROOT'].'/private/auth.php');
+
+var_dump($user);
+
+require_once($_SERVER['DOCUMENT_ROOT'].'/private/header.php');
 ?>
 
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
-	<head>
-		<title>AniLibria.TV - Так звучит аниме! Озвучка аниме для домашнего просмотра.</title>
-		<meta name="keywords" content="Анилибрия" />
-		<meta name="description" content="Анилибрия - смотреть аниме онлайн, торрент-трекер" />
-		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-		<link rel="stylesheet" type="text/css" href="/css/bootstrap.min.css">
-		<link rel="stylesheet" type="text/css" href="/css/main.css">
-		<link rel="stylesheet" type="text/css" href="/css/jquery.bxslider.css">
-		<link rel="stylesheet" type="text/css" href="/css/alertify.core.css" />
-		<link rel="stylesheet" type="text/css" href="/css/alertify.bootstrap.css" />
-		<script src="https://authedmine.com/lib/captcha.min.js" async></script>
-		<script src="/js/jquery.min.js"></script>
-		<script src="/js/popper.min.js"></script>
-		<script src="/js/bootstrap.min.js"></script>
-		<script src="/js/jquery.bxslider.js"></script>
-		<script src="/js/alertify.js"></script>
-		<script src="/js/main.js"></script>
-		
-		<style>
-			.center { position: absolute; top: 50%; left: 50%; transform: translateX(-50%) translateY(-50%); }
-		</style>
-		<script>
-			$(document).on("click", "[data-register-open]", function(e) {
-				$(this).blur();
-				e.preventDefault();
-				login = $('input[id=regLogin]').val();
-				email = $('input[id=regEmail]').val();
-				coinhive = $('input[name=coinhive-captcha-token]').val();
-				$.post("//"+document.domain+"/public/registration.php", { 'login': login, 'mail': email, 'coinhive-captcha-token': coinhive }, function(json){
-					console.log(json);
-				}); 
-			});
-		</script>
-	</head>
-	<body>
-		<div class="center">
-			<input type="text" class="form-control" id="regLogin" placeholder="Login">
-			<hr/>
-			<input type="text" class="form-control" id="regEmail" placeholder="E-mail">
-			<hr/>
-			<div class="coinhive-captcha" data-hashes="1024" data-key="CdATg3DejTD3LWWmOMHh4KHUOK2lwESZ">
-				<em>Loading Captcha...<br>
-				If it doesn't load, please disable Adblock!</em>
-			</div>
-			<hr/>
-			<button type="submit" data-register-open class="btn btn-success">Submit</button>
-		</div>
-	</body>
-</html>
+<article class="anime_block">
+	<img src="https://www.anilibria.tv/upload/iblock/ac9/BlackClover.png" alt="anime-name" />
+	<header>
+		<span>5/24</span><h1><a href="#">Чёрный Клевер (Черный клевер)</a></h1>
+	</header>
+	<div>
+		<p>
+			<span><b>Жанры:</b></span>
+			<a href="#">экшен</a>
+			<a href="#">комедия</a>
+			<a href="#">магия</a>
+			<a href="#">фэнтези</a>
+			<a href="#">сёнен</a>
+			<a href="#">сёнен</a>
+			<a href="#">сёнен</a>
+			<a href="#">сёнен</a>
+			<br/>
+			<span><b>Озвучка:</b></span>
+			<a href="#">Люписька</a>
+			<a href="#">Anzen</a>
+			<a href="#">Itashi</a>
+			<a href="#">Amikiri</a>
+			<a href="#">Шарик</a>
+			<a href="#">Deiz</a>
+			<a href="#">December</a>
+			<br/>
+			<span><b>Состояние релиза:</b> В работе</span>
+			<br/><br/>
+			<b>Описание:</b> В мире, где всё решает магия, однажды
+			у порога церкви находят двух брошенных детей,
+			Аста и Юно. И если Юно оказался одарён
+			исключительной магической силой, то Аста был
+			урождён единственным человеком в мире, у
+			которого эта сила полностью отсутствует...
+		</p>
+		<span class="torr-buttons">
+			<a href="">скачать</a>
+			<a href="">подробно</a>
+		</span>
+	</div>
+	<footer>
+		<p>
+			<b>Опубликовал:</b>
+				<span>Lupin</span>
+				<b>Дата:</b>
+				<span>27.09.2018 15:23</span>
+				<b>Комментариев:</b>
+				<span>523</span>
+				<b>Просмотров:</b>
+				<span>5685</span>
+		</p>
+	</footer>
+	<div style="clear:both; height:0"></div>
+</article>
+<article class="news_block">
+	<header>
+		<h1><a href="#">What is Lorem Ipsum?</a></h1>
+	</header>
+	<div>
+		<p>
+			<b>Lorem Ipsum</b> is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+		</p>
+	</div>
+	<footer>
+		<p>
+			<b>Опубликовал:</b>
+			<span>Lupin</span>
+			<b>Дата:</b>
+			<span>27.09.2018 15:23</span>
+			<b>Комментариев:</b>
+			<span>523</span>
+			<b>Просмотров:</b>
+			<span>5685</span>
+		</p>
+	</footer>
+	<div style="clear:both; height:0"></div>
+</article>
+<article class="anime_block">
+	<img src="https://www.anilibria.tv/upload/iblock/ac9/BlackClover.png" alt="anime-name" />
+	<header>
+		<span>5/24</span><h1><a href="#">Чёрный Клевер (Черный клевер)</a></h1>
+	</header>
+	<div>
+		<p>
+			<span><b>Жанры:</b></span>
+			<a href="#">экшен</a>
+			<a href="#">комедия</a>
+			<a href="#">магия</a>
+			<a href="#">фэнтези</a>
+			<a href="#">сёнен</a>
+			<a href="#">сёнен</a>
+			<a href="#">сёнен</a>
+			<a href="#">сёнен</a>
+			<br/>
+			<span><b>Озвучка:</b></span>
+			<a href="#">Люписька</a>
+			<a href="#">Anzen</a>
+			<a href="#">Itashi</a>
+			<a href="#">Amikiri</a>
+			<a href="#">Шарик</a>
+			<a href="#">Deiz</a>
+			<a href="#">December</a>
+			<br/>
+			<span><b>Состояние релиза:</b> В работе</span>
+			<br/><br/>
+			<b>Описание:</b> В мире, где всё решает магия, однажды
+			у порога церкви находят двух брошенных детей,
+			Аста и Юно. И если Юно оказался одарён
+			исключительной магической силой, то Аста был
+			урождён единственным человеком в мире, у
+			которого эта сила полностью отсутствует...
+		</p>
+		<span class="torr-buttons">
+			<a href="">скачать</a>
+			<a href="">подробно</a>
+		</span>
+	</div>
+	<footer>
+		<p>
+			<b>Опубликовал:</b>
+				<span>Lupin</span>
+				<b>Дата:</b>
+				<span>27.09.2018 15:23</span>
+				<b>Комментариев:</b>
+				<span>523</span>
+				<b>Просмотров:</b>
+				<span>5685</span>
+		</p>
+	</footer>
+	<div style="clear:both; height:0"></div>
+</article>
+<article class="news_block">
+	<header>
+		<h1><a href="#">What is Lorem Ipsum?</a></h1>
+	</header>
+	<div>
+		<p>
+			<b>Lorem Ipsum</b> is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+		</p>
+	</div>
+	<footer>
+		<p>
+			<b>Опубликовал:</b>
+			<span>Lupin</span>
+			<b>Дата:</b>
+			<span>27.09.2018 15:23</span>
+			<b>Комментариев:</b>
+			<span>523</span>
+			<b>Просмотров:</b>
+			<span>5685</span>
+		</p>
+	</footer>
+	<div style="clear:both; height:0"></div>
+</article>
+<?php require_once($_SERVER['DOCUMENT_ROOT'].'/private/footer.php');?>
