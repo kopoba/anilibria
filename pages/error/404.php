abf977eb3b8ad4b6ead3d2af9e3e931e53ed1fb1
poiuty <poiuty@lepus.su>
2019-01-14 14:15:56 +0300

    add error pages

---
+++ pages/error/404.php
@@ -0,0 +1,41 @@
+<?php
+require($_SERVER['DOCUMENT_ROOT'].'/private/config.php');
+require($_SERVER['DOCUMENT_ROOT'].'/private/init/mysql.php');
+require($_SERVER['DOCUMENT_ROOT'].'/private/init/memcache.php');
+require($_SERVER['DOCUMENT_ROOT'].'/private/init/session.php');
+require($_SERVER['DOCUMENT_ROOT'].'/private/init/var.php');
+require($_SERVER['DOCUMENT_ROOT'].'/private/func.php');
+require($_SERVER['DOCUMENT_ROOT'].'/private/auth.php');
+require($_SERVER['DOCUMENT_ROOT'].'/private/minify.php');
+
+$var['page'] = '404';
+
+header('HTTP/1.0 404 Not Found');
+
+require($_SERVER['DOCUMENT_ROOT'].'/private/header.php');
+?>
+
+<div class="news-block">
+	<div>
+		<center>		
+			<img src="/img/404.png">
+		</center>
+	</div>
+	<div class="clear"></div>
+	<div class="news_footer"></div>
+</div>
+
+<!-- Put this script tag to the <head> of your page -->
+<script type="text/javascript" src="https://vk.com/js/api/openapi.js?160"></script>
+
+<script type="text/javascript">
+  VK.init({apiId: 6798605, onlyWidgets: true});
+</script>
+
+<!-- Put this div tag to the place, where the Comments block will be -->
+<div id="vk_comments" style="margin-top: 15px;"></div>
+<script type="text/javascript">
+VK.Widgets.Comments("vk_comments", {limit: 5, pageUrl: "/pages/error/404.php", attach: false});
+</script>
+
+<?require($_SERVER['DOCUMENT_ROOT'].'/private/footer.php');?>
