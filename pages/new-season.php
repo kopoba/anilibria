f4dc17c48e5020528425b4446cc498e100927523
Kirjaa <kirillik1993@gmail.com>
2019-03-14 21:04:09 +0200

    upcoming voting update

--- pages/new-season.php
+++ pages/new-season.php
@@ -8,13 +8,17 @@ require($_SERVER['DOCUMENT_ROOT'].'/private/init/var.php');
 require($_SERVER['DOCUMENT_ROOT'].'/private/func.php');
 require($_SERVER['DOCUMENT_ROOT'].'/private/auth.php');
 
-$var['title'] = 'Новый сезон';
+$var['title'] = 'Анонс весны 2019';
 $var['page'] = 'new-season';
 
 require($_SERVER['DOCUMENT_ROOT'].'/private/header.php');
 ?>
 
-<div class="news-block">	
+<div class="news-block">
+    <div id="upcoming_page_heading">
+        <h2>Анонс аниме-весна 2019 года</h2>
+        <p>Внимание! Голосовать могут только авторизованные пользователи.</p>
+    </div>
 	<div>
 		<?php echo showNewSeason(); ?>
 	</div>
