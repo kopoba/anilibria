2ec3d838bd8376a12d02f341124f8a06cc20ef46
Evgeny Nizamiev <radiationx@yandex.ru>
2019-03-25 22:25:57 +0500

    app update 44 2.3.4

--- private/init/var.php
+++ private/init/var.php
@@ -12,7 +12,7 @@ if(!empty($_SERVER['HTTP_USER_AGENT'])){
 }
 $var['default_user_values'] = '{"name":"","age":"","sex":"","vk":"","telegram":"","steam":"","phone":"","skype":"","facebook":"","instagram":"","youtube":"","twitch":"","twitter":""}';
 
-$var['app_version'] = 43;
+$var['app_version'] = 44;
 
 $var['sex'] = [
 	'Не указано',
