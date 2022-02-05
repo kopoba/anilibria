1a3b521b5100410f44670a95db1e3a15c88b87ac
T1MOXA <danilevsky_tima@ukr.net>
2022-02-06 01:58:11 +0200

    Исправление кроссдоменной авторизации

--- private/func.php
+++ private/func.php
@@ -476,6 +476,8 @@ function login() // DONE
 
     $hash = startSession($row);
 
+    header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Allow-Methods: POST');
     _message('success', 'ok', ['sessionId' => $hash]);
 
 }
