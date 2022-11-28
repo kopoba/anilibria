f88cd0a49738dc2434200560d899e35b967548f3
pavloniym <pavloniym@gmail.com>
2022-11-28 21:10:04 +0300

    fix(App): fixed lottery for new cache server

--- pages/cacheLottery.php
+++ pages/cacheLottery.php
@@ -1,7 +1,20 @@
 <?php
 
-require($_SERVER['DOCUMENT_ROOT'].'/private/config.php');
-require($_SERVER['DOCUMENT_ROOT'].'/private/init/var.php');
-require($_SERVER['DOCUMENT_ROOT'].'/private/init/memcache.php');
+require($_SERVER['DOCUMENT_ROOT'] . '/private/config.php');
+require($_SERVER['DOCUMENT_ROOT'] . '/private/init/mysql.php');
+require($_SERVER['DOCUMENT_ROOT'] . '/private/init/memcache.php');
+require($_SERVER['DOCUMENT_ROOT'] . '/private/init/session.php');
+require($_SERVER['DOCUMENT_ROOT'] . '/private/init/var.php');
+require($_SERVER['DOCUMENT_ROOT'] . '/private/func.php');
+require($_SERVER['DOCUMENT_ROOT'] . '/private/auth.php');
 
-$cache->set('cacheLottery', (int)$_GET['percentage'], 0);
+
+echo '<pre>';
+echo sprintf('Previous value: %s', $cache->get('cacheLottery'));
+echo PHP_EOL;
+
+if (in_array($user['id'], ['2', '368751', '249035'])) {
+    $cache->set('cacheLottery', (int)$_GET['percentage'], 0);
+}
+
+echo sprintf('Current value: %s', $cache->get('cacheLottery'));
