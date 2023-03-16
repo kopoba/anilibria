f2a4385a332b5209b642a30cd06c71d2ee16fd29
kopoba <kopoba.webmaster@gmail.com>
2023-03-17 00:46:25 +0400

    conflictfixthirdattempt

--- private/header.php
+++ private/header.php
@@ -87,6 +87,7 @@ $xcss = headerAds();
 		</script>
 
 		<script src="<?php echo '/js/theme-toggle.js';?>"></script>
+		
 		<!-- Global site tag (gtag.js) - Google Analytics -->
 		<script async src="https://www.googletagmanager.com/gtag/js?id=<?php global $conf; echo $conf['google_analytics_id']; ?>"></script>
 		<script>
